7b3072eec420c83ac5978a54a9d92c6386df36d3
David Walluck <dwalluck@redhat.com>
2013-10-10 15:38:08 -0400

    dom

--- src/org/apache/html/dom/HTMLObjectElementImpl.java
+++ src/org/apache/html/dom/HTMLObjectElementImpl.java
@@ -16,6 +16,8 @@
  */
 package org.apache.html.dom;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.html.HTMLObjectElement;
 
 /**
@@ -31,6 +33,10 @@ public class HTMLObjectElementImpl
 {
     
     private static final long serialVersionUID = 2276953229932965067L;
+
+    public Document getContentDocument() {
+        throw new DOMException(DOMException.NOT_SUPPORTED_ERR, "Method not supported");
+    }
 
     public String getCode()
     {
